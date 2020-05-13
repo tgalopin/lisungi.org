9cf11718e7d79155631cebeb0bc3d160f7b9ae4d
Titouan Galopin <galopintitouan@gmail.com>
2020-05-13 22:39:12 +0200

    Implement helper and request forms

--- tests/Controller/HelpRequestControllerTest.php
+++ tests/Controller/HelpRequestControllerTest.php
@@ -3,16 +3,81 @@
 namespace App\Tests\Controller;
 
 use App\Entity\HelpRequest;
+use App\MatchFinder\Locality;
 use App\Repository\HelpRequestRepository;
 use App\Tests\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
 
 class HelpRequestControllerTest extends WebTestCase
 {
+    public function testRequest()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/process/j-ai-besoin-d-aide');
+        $this->assertResponseIsSuccessful();
+
+        $button = $crawler->selectButton('Envoyer ma demande');
+        $this->assertCount(1, $button);
+
+        $form = $button->form();
+        $form->setValues([
+            'composite_help_request[firstName]' => 'Titouan',
+            'composite_help_request[lastName]' => 'Galopin',
+            'composite_help_request[email]' => 'titouan.galopin@example.com',
+            'composite_help_request[phone]' => '022 281 4578',
+            'composite_help_request[locality]' => Locality::LOCALITIES['fr_CD']['KALAMU'],
+            'composite_help_request[organization]' => 'Citipo',
+        ]);
+
+        // gets the raw values
+        $values = $form->getPhpValues();
+
+        // adds fields to the raw values
+        // see https://symfony.com/doc/current/testing.html#adding-and-removing-forms-to-a-collection
+        $values['composite_help_request']['details'] = [
+            HelpRequest::TYPE_MASKS => ['need' => true, 'quantity' => 1, 'details' => ''],
+            HelpRequest::TYPE_GLASSES => ['need' => true, 'quantity' => 2, 'details' => ''],
+            HelpRequest::TYPE_BLOUSES => ['need' => true, 'quantity' => 3, 'details' => ''],
+            HelpRequest::TYPE_GEL => ['need' => true, 'quantity' => 4, 'details' => ''],
+            HelpRequest::TYPE_GLOVES => ['need' => true, 'quantity' => 5, 'details' => ''],
+            HelpRequest::TYPE_SOAP => ['need' => true, 'quantity' => 6, 'details' => ''],
+            HelpRequest::TYPE_FOOD => ['need' => true, 'quantity' => '', 'details' => ''],
+            HelpRequest::TYPE_OTHER => ['need' => true, 'quantity' => '', 'details' => 'Other'],
+        ];
+
+        // submits the form with the existing and new values
+        $client->request($form->getMethod(), $form->getUri(), $values, $form->getPhpFiles());
+
+        $expectedRequests = [
+            ['type' => HelpRequest::TYPE_MASKS, 'quantity' => 1, 'details' => ''],
+            ['type' => HelpRequest::TYPE_GLASSES, 'quantity' => 2, 'details' => ''],
+            ['type' => HelpRequest::TYPE_BLOUSES, 'quantity' => 3, 'details' => ''],
+            ['type' => HelpRequest::TYPE_GEL, 'quantity' => 4, 'details' => ''],
+            ['type' => HelpRequest::TYPE_GLOVES, 'quantity' => 5, 'details' => ''],
+            ['type' => HelpRequest::TYPE_SOAP, 'quantity' => 6, 'details' => ''],
+            ['type' => HelpRequest::TYPE_FOOD, 'quantity' => '', 'details' => ''],
+            ['type' => HelpRequest::TYPE_OTHER, 'quantity' => '', 'details' => 'Other'],
+        ];
+
+        /** @var HelpRequest[] $requests */
+        $requests = self::$container->get(HelpRequestRepository::class)->findBy(['email' => 'titouan.galopin@example.com']);
+
+        foreach ($requests as $key => $request) {
+            $this->assertInstanceOf(HelpRequest::class, $request);
+            $this->assertSame('Titouan', $request->firstName);
+            $this->assertSame('Galopin', $request->lastName);
+            $this->assertSame(Locality::LOCALITIES['fr_CD']['KALAMU'], $request->locality);
+            $this->assertSame('titouan.galopin@example.com', $request->email);
+            $this->assertSame('022 281 4578', $request->phone);
+            $this->assertSame($expectedRequests[$key]['type'], $request->type);
+            $this->assertSame($expectedRequests[$key]['quantity'], $request->quantity);
+            $this->assertSame($expectedRequests[$key]['details'], $request->details);
+        }
+    }
+
     public function testRequesterViewDelete()
     {
-        $this->markTestSkipped('To reimplement');
-
         $client = static::createClient();
 
         $request = self::$container->get(HelpRequestRepository::class)->findOneBy(['email' => 'jeanne.martin@example.com']);
@@ -33,65 +98,5 @@ class HelpRequestControllerTest extends WebTestCase
 
         $request = self::$container->get(HelpRequestRepository::class)->findOneBy(['email' => 'jeanne.martin@example.com']);
         $this->assertNull($request);
-    }
-
-    public function testRequest()
-    {
-        $this->markTestSkipped('To reimplement');
-
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/process/j-ai-besoin-d-aide');
-        $this->assertResponseIsSuccessful();
-
-        $button = $crawler->selectButton('Envoyer ma demande');
-        $this->assertCount(1, $button);
-
-        $form = $button->form();
-        $form->setValues([
-            'composite_help_request[firstName]' => 'Titouan',
-            'composite_help_request[lastName]' => 'Galopin',
-            'composite_help_request[zipCode]' => 75008,
-            'composite_help_request[email]' => 'titouan.galopin@example.com',
-            'composite_help_request[jobType]' => 'health',
-            'composite_help_request[confirm]' => 1,
-        ]);
-
-        // gets the raw values
-        $values = $form->getPhpValues();
-
-        // adds fields to the raw values
-        // see https://symfony.com/doc/current/testing.html#adding-and-removing-forms-to-a-collection
-        $values['composite_help_request']['details'] = [
-            ['helpType' => HelpRequest::TYPE_GROCERIES],
-            ['helpType' => HelpRequest::TYPE_BABYSIT, 'childAgeRange' => HelpRequest::AGE_RANGE_35],
-            ['helpType' => HelpRequest::TYPE_BABYSIT, 'childAgeRange' => HelpRequest::AGE_RANGE_69],
-        ];
-
-        // submits the form with the existing and new values
-        $client->request($form->getMethod(), $form->getUri(), $values, $form->getPhpFiles());
-
-        $help_request = self::$container->get(HelpRequestRepository::class)->findOneBy(['email' => 'titouan.galopin@example.com', 'childAgeRange' => HelpRequest::AGE_RANGE_35]);
-        $this->assertInstanceOf(HelpRequest::class, $help_request);
-        $this->assertSame('Titouan', $help_request->firstName);
-        $this->assertSame('Galopin', $help_request->lastName);
-        $this->assertSame('75008', $help_request->zipCode);
-        $this->assertSame('titouan.galopin@example.com', $help_request->email);
-        $this->assertSame('health', $help_request->jobType);
-        $this->assertSame(HelpRequest::AGE_RANGE_35, $help_request->childAgeRange);
-        $this->assertSame(HelpRequest::TYPE_BABYSIT, $help_request->helpType);
-
-        $help_request = self::$container->get(HelpRequestRepository::class)->findOneBy(['email' => 'titouan.galopin@example.com', 'childAgeRange' => HelpRequest::AGE_RANGE_69]);
-        $this->assertInstanceOf(HelpRequest::class, $help_request);
-        $this->assertSame('Titouan', $help_request->firstName);
-        $this->assertSame('Galopin', $help_request->lastName);
-        $this->assertSame('75008', $help_request->zipCode);
-        $this->assertSame('titouan.galopin@example.com', $help_request->email);
-        $this->assertSame('health', $help_request->jobType);
-        $this->assertSame(HelpRequest::AGE_RANGE_69, $help_request->childAgeRange);
-        $this->assertSame(HelpRequest::TYPE_BABYSIT, $help_request->helpType);
-
-        $help_request = self::$container->get(HelpRequestRepository::class)->findOneBy(['email' => 'titouan.galopin@example.com', 'helpType' => HelpRequest::TYPE_GROCERIES]);
-        $this->assertInstanceOf(HelpRequest::class, $help_request);
     }
 }
--- tests/Controller/HelperControllerTest.php
+++ tests/Controller/HelperControllerTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Controller;
 
 use App\Entity\Helper;
+use App\MatchFinder\Locality;
 use App\Repository\HelperRepository;
 use App\Tests\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,8 +12,6 @@ class HelperControllerTest extends WebTestCase
 {
     public function testHelperCreate()
     {
-        $this->markTestSkipped('To reimplement');
-
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/process/je-peux-aider');
@@ -24,36 +23,47 @@ class HelperControllerTest extends WebTestCase
         $client->submit($button->form(), [
             'helper[firstName]' => 'Titouan',
             'helper[lastName]' => 'Galopin',
-            'helper[age]' => '25',
-            'helper[zipCode]' => '75008',
+            'helper[locality]' => Locality::LOCALITIES['fr_CD']['KALAMU'],
             'helper[email]' => 'titouan.galopin@example.com',
-            'helper[canBuyGroceries]' => true,
-            'helper[canBabysit]' => true,
-            'helper[haveChildren]' => true,
-            'helper[babysitMaxChildren]' => 3,
-            'helper[babysitAgeRanges]' => ['0-1'],
+            'helper[phone]' => '022 281 4578',
+            'helper[company]' => 'Citipo',
+            'helper[masks]' => 1,
+            'helper[glasses]' => 2,
+            'helper[blouses]' => 3,
+            'helper[gel]' => 4,
+            'helper[gloves]' => 5,
+            'helper[soap]' => 6,
+            'helper[food]' => 'Food',
+            'helper[other]' => 'Other',
             'helper[confirm]' => true,
+            'helper[confirm_legal]' => true,
         ]);
         $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
 
+        $client->followRedirect();
+        $this->assertResponseIsSuccessful();
+
+        /** @var Helper $helper */
         $helper = self::$container->get(HelperRepository::class)->findOneBy(['email' => 'titouan.galopin@example.com']);
         $this->assertInstanceOf(Helper::class, $helper);
         $this->assertSame('Titouan', $helper->firstName);
         $this->assertSame('Galopin', $helper->lastName);
-        $this->assertSame(25, $helper->age);
-        $this->assertSame('75008', $helper->zipCode);
         $this->assertSame('titouan.galopin@example.com', $helper->email);
-        $this->assertSame(true, $helper->canBuyGroceries);
-        $this->assertSame(true, $helper->canBabysit);
-        $this->assertSame(true, $helper->haveChildren);
-        $this->assertSame(3, $helper->babysitMaxChildren);
-        $this->assertSame(['0-1'], $helper->babysitAgeRanges);
+        $this->assertSame('022 281 4578', $helper->phone);
+        $this->assertSame(Locality::LOCALITIES['fr_CD']['KALAMU'], $helper->locality);
+        $this->assertSame('Citipo', $helper->company);
+        $this->assertSame(1, $helper->masks);
+        $this->assertSame(2, $helper->glasses);
+        $this->assertSame(3, $helper->blouses);
+        $this->assertSame(4, $helper->gel);
+        $this->assertSame(5, $helper->gloves);
+        $this->assertSame(6, $helper->soap);
+        $this->assertSame('Food', $helper->food);
+        $this->assertSame('Other', $helper->other);
     }
 
     public function testHelperRequiresConfirm()
     {
-        $this->markTestSkipped('To reimplement');
-
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/process/je-peux-aider');
@@ -65,22 +75,24 @@ class HelperControllerTest extends WebTestCase
         $client->submit($button->form(), [
             'helper[firstName]' => 'Titouan',
             'helper[lastName]' => 'Galopin',
-            'helper[age]' => '25',
-            'helper[zipCode]' => '75008',
+            'helper[locality]' => Locality::LOCALITIES['fr_CD']['KALAMU'],
             'helper[email]' => 'titouan.galopin@example.com',
-            'helper[canBuyGroceries]' => true,
-            'helper[canBabysit]' => true,
-            'helper[haveChildren]' => true,
-            'helper[babysitMaxChildren]' => 3,
-            'helper[babysitAgeRanges]' => ['0-1'],
+            'helper[phone]' => '022 281 4578',
+            'helper[company]' => 'Citipo',
+            'helper[masks]' => 1,
+            'helper[glasses]' => 2,
+            'helper[blouses]' => 3,
+            'helper[gel]' => 4,
+            'helper[gloves]' => 5,
+            'helper[soap]' => 6,
+            'helper[food]' => 'Food',
+            'helper[other]' => 'Other',
         ]);
         $this->assertResponseIsSuccessful($client->getResponse()->getStatusCode());
     }
 
     public function testHelperViewDelete()
     {
-        $this->markTestSkipped('To reimplement');
-
         $client = static::createClient();
 
         $helper = self::$container->get(HelperRepository::class)->findOneBy(['email' => 'elizabeth.gregory@example.com']);
